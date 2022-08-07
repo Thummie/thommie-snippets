49e8c85c3c3d08da1defd5c432dbc26ab82c921e
Thommie <53064581+Thummie@users.noreply.github.com>
2022-08-07 14:18:03 +0200

    Add files via upload

---
+++ cl_main.lua
@@ -0,0 +1,39 @@
+-- Disable Blind Fire while in cover
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(4)
+		local ped = GetPlayerPed(-1)
+		if IsPedInCover(ped) and not IsPedAimingFromCover(ped) then
+			DisableControlAction(2, 24, true)
+			DisableControlAction(2, 142, true)
+			DisableControlAction(2, 257, true)
+		end
+	end
+end)
+
+-- Disable Blind Fire while not in cover
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(4)
+		local ped = GetPlayerPed(-1)
+		if not IsPlayerFreeAiming(ped) then
+			DisableControlAction(2, 24, true)
+			DisableControlAction(2, 142, true)
+			DisableControlAction(2, 257, true)
+		end
+	end
+end)
+
+-- Disable Pistol Whip
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(4)
+		if IsPedArmed(GetPlayerPed(-1), 6) then
+			DisableControlAction(1, 140, true)
+			DisableControlAction(1, 141, true)
+			DisableControlAction(1, 142, true)
+		else
+			Citizen.Wait(1500)
+		end
+	end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'adamant'
+game 'gta5'
+
+description 'Disable blind fire in fivem.'
+author 'Thommie'
+
+client_script 'cl_main.lua'
